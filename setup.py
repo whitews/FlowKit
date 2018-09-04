2d13b61e4dc5d6a3a1f288bcd7e5ed30a3004fa2
Scott White <whitews@gmail.com>
2018-09-04 15:23:11 -0400

    bump version: 0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='FlowKit',
-    version='0.0.3',
+    version='0.0.4',
     packages=['flowkit', 'flowkit.models'],
     package_data={'': []},
     description='Flow Cytometry Toolkit',
