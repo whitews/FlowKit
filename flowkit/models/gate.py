8e2a279206c3e696178c250ce0e5c8a54a5d0bd5
whitews <whitews@gmail.com>
2019-01-16 12:27:49 -0500

    first working version of RectangleGate (at least as a range gate)

--- flowkit/models/gate.py
+++ flowkit/models/gate.py
@@ -1,7 +1,9 @@
 import pkgutil
 import os
+from abc import ABC, abstractmethod
 # noinspection PyUnresolvedReferences
 from lxml import etree, objectify
+import numpy as np
 
 loader = pkgutil.get_loader('flowkit.resources')
 resource_path = os.path.dirname(loader.path)
@@ -62,7 +64,7 @@ class Dimension(object):
             )
 
 
-class Gate(object):
+class Gate(ABC):
     """
     Represents a single flow cytometry gate
     """
@@ -91,6 +93,10 @@ class Gate(object):
             dim = Dimension(dim_el, gating_namespace, data_type_namespace)
             self.dimensions.append(dim)
 
+    @abstractmethod
+    def apply(self, events, pnn_labels):
+        pass
+
 
 class RectangleGate(Gate):
     """
@@ -108,6 +114,37 @@ class RectangleGate(Gate):
     def __init__(self, gate_element, gating_namespace, data_type_namespace):
         super().__init__(gate_element, gating_namespace, data_type_namespace)
         pass
+
+    def apply(self, events, pnn_labels):
+        if events.shape[1] != len(pnn_labels):
+            raise ValueError(
+                "Number of FCS dimensions (%d) does not match label count (%d)"
+                % (events.shape[1], len(pnn_labels))
+            )
+
+        dim_idx = []
+        dim_min = []
+        dim_max = []
+
+        for dim in self.dimensions:
+            if dim.min is None and dim.max is None:
+                raise ValueError(
+                    "Gate '%s' does not include a min or max value" % self.id
+                )
+
+            dim_idx.append(pnn_labels.index(dim.label))
+            dim_min.append(dim.min)
+            dim_max.append(dim.max)
+
+        results = np.ones(events.shape[0], dtype=np.bool)
+
+        for i, d_idx in enumerate(dim_idx):
+            if dim_min[i] is not None:
+                results = np.bitwise_and(results, events[:, d_idx] >= dim_min[i])
+            if dim_max[i] is not None:
+                results = np.bitwise_and(results, events[:, d_idx] < dim_max[i])
+
+        return results
 
 
 class GatingStrategy(object):
@@ -145,7 +182,8 @@ class GatingStrategy(object):
             ':'.join([self._gating_ns, gt]) for gt in GATE_TYPES
         ]
 
-        self.gates = []
+        # keys will be gate ID, value is the gate object itself
+        self.gates = {}
 
         for gt in self._gate_types:
             gt_gates = root.findall(gt, namespace_map)
@@ -154,7 +192,35 @@ class GatingStrategy(object):
                 constructor = globals()[gt.split(':')[1]]
                 g = constructor(gt_gate, self._gating_ns, self._data_type_ns)
 
-                self.gates.append(g)
+                if g.id in self.gates:
+                    raise ValueError(
+                        "Gate '%s' already exists. "
+                        "Duplicate gate IDs are not allowed." % g.id
+                    )
+                self.gates[g.id] = g
 
-    def gate_sample(self, sample, gate):
-        pass
+    def gate_sample(self, sample, gate_id):
+        """
+        Apply a gate to a sample, returning a dictionary where gate ID is the
+        key, and the value contains the event indices for events in the Sample
+        which are contained by the gate. If the gate has a parent gate, all
+        gates in the hierarchy above will be included in the results. If 'gate'
+        is None, then all gates will be evaluated.
+
+        :param sample: an FCS Sample instance
+        :param gate_id: A gate ID to evaluate on given Sample. If None, all gates
+            will be evaluated
+        :return: Dictionary where keys are gate IDs, values are event indices
+            in the given Sample which are contained by the gate
+        """
+        if gate_id is None:
+            gates = self.gates
+        else:
+            gates = [self.gates[gate_id]]
+
+        results = {}
+
+        for gate in gates:
+            results[gate_id] = gate.apply(sample.get_raw_events(), sample.pnn_labels)
+
+        return results
