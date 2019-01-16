71ef7b5c83cc1bcf7dae288f6948d78ff538d41c
whitews <whitews@gmail.com>
2019-01-16 14:35:33 -0500

    add gating unit tests...full RectangleGate working now

---
+++ unit_test/gating_unit_tests.py
@@ -0,0 +1,46 @@
+import unittest
+import sys
+import os
+import numpy as np
+
+sys.path.append(os.path.abspath('..'))
+
+from flowkit import Sample, GatingStrategy
+
+
+class RangeGateTestCase(unittest.TestCase):
+    @staticmethod
+    def test_min_range_gate():
+        gml_path = '/home/swhite/git/flowkit/examples/gate_ref/gml_range_gate.xml'
+        fcs_path = '/home/swhite/git/flowkit/examples/gate_ref/data1.fcs'
+        res_path = '/home/swhite/git/flowkit/examples/gate_ref/Results_Range1.txt'
+
+        gs = GatingStrategy(gml_path)
+        sample = Sample(
+            fcs_path,
+            filter_anomalous_events=False,
+            filter_negative_scatter=False
+        )
+        truth = np.loadtxt(res_path, dtype=np.bool)
+
+        result = gs.gate_sample(sample, 'Range1')
+
+        np.testing.assert_array_equal(truth, result['Range1'])
+
+    @staticmethod
+    def test_rect1_gate():
+        gml_path = '/home/swhite/git/flowkit/examples/gate_ref/gml_rect1_gate.xml'
+        fcs_path = '/home/swhite/git/flowkit/examples/gate_ref/data1.fcs'
+        res_path = '/home/swhite/git/flowkit/examples/gate_ref/Results_Rectangle1.txt'
+
+        gs = GatingStrategy(gml_path)
+        sample = Sample(
+            fcs_path,
+            filter_anomalous_events=False,
+            filter_negative_scatter=False
+        )
+        truth = np.loadtxt(res_path, dtype=np.bool)
+
+        result = gs.gate_sample(sample, 'Rectangle1')
+
+        np.testing.assert_array_equal(truth, result['Rectangle1'])
--- unit_test/unit_tests.py
+++ unit_test/unit_tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from .sample_unit_tests import LoadSampleTestCase
+from .gating_unit_tests import RangeGateTestCase
 
 if __name__ == "__main__":
     unittest.main()
