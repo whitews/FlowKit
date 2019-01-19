d8355d4dcc406259707950a3a0393c2c3389f20b
whitews <whitews@gmail.com>
2019-01-19 15:43:14 -0500

    add 2nd test for PolygonGate

--- unit_test/gating_unit_tests.py
+++ unit_test/gating_unit_tests.py
@@ -82,6 +82,24 @@ class RangeGateTestCase(unittest.TestCase):
         np.testing.assert_array_equal(truth, result['Polygon1'])
 
     @staticmethod
+    def test_poly2_gate():
+        gml_path = 'examples/gate_ref/gml_poly2_gate.xml'
+        fcs_path = 'examples/gate_ref/data1.fcs'
+        res_path = 'examples/gate_ref/Results_Polygon2.txt'
+
+        gs = GatingStrategy(gml_path)
+        sample = Sample(
+            fcs_path,
+            filter_anomalous_events=False,
+            filter_negative_scatter=False
+        )
+        truth = np.loadtxt(res_path, dtype=np.bool)
+
+        result = gs.gate_sample(sample, 'Polygon2')
+
+        np.testing.assert_array_equal(truth, result['Polygon2'])
+
+    @staticmethod
     def test_ellipse1_gate():
         gml_path = 'examples/gate_ref/gml_ellipse1_gate.xml'
         fcs_path = 'examples/gate_ref/data1.fcs'
