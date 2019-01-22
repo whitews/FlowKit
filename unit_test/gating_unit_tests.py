cf05edf4719f0688c978aada6e435b238ccfce93
whitews <whitews@gmail.com>
2019-01-22 14:35:54 -0500

    add 2nd test for ratio range gate

--- unit_test/gating_unit_tests.py
+++ unit_test/gating_unit_tests.py
@@ -197,3 +197,21 @@ class RangeGateTestCase(unittest.TestCase):
         result = gs.gate_sample(sample, 'RatRange1')
 
         np.testing.assert_array_equal(truth, result['RatRange1'])
+
+    @staticmethod
+    def test_ratio_range2_gate():
+        gml_path = 'examples/gate_ref/gml_ratio_range2_gate.xml'
+        fcs_path = 'examples/gate_ref/data1.fcs'
+        res_path = 'examples/gate_ref/Results_RatRange2.txt'
+
+        gs = GatingStrategy(gml_path)
+        sample = Sample(
+            fcs_path,
+            filter_anomalous_events=False,
+            filter_negative_scatter=False
+        )
+        truth = np.loadtxt(res_path, dtype=np.bool)
+
+        result = gs.gate_sample(sample, 'RatRange2')
+
+        np.testing.assert_array_equal(truth, result['RatRange2'])
