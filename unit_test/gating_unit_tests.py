81509e17a0b1c1286013283229305d7554136fd4
whitews <whitews@gmail.com>
2019-01-23 11:00:07 -0500

    test & data for boolean 'or' operation & 'use-as-complement' option

--- unit_test/gating_unit_tests.py
+++ unit_test/gating_unit_tests.py
@@ -299,3 +299,39 @@ class GatingMLTestCase(unittest.TestCase):
         result = gs.gate_sample(sample, 'And2')
 
         np.testing.assert_array_equal(truth, result['And2'])
+
+    @staticmethod
+    def test_boolean_or1_gate():
+        gml_path = 'examples/gate_ref/gml_boolean_or1_gate.xml'
+        fcs_path = 'examples/gate_ref/data1.fcs'
+        res_path = 'examples/gate_ref/Results_Or1.txt'
+
+        gs = GatingStrategy(gml_path)
+        sample = Sample(
+            fcs_path,
+            filter_anomalous_events=False,
+            filter_negative_scatter=False
+        )
+        truth = np.loadtxt(res_path, dtype=np.bool)
+
+        result = gs.gate_sample(sample, 'Or1')
+
+        np.testing.assert_array_equal(truth, result['Or1'])
+
+    @staticmethod
+    def test_boolean_and3_complement_gate():
+        gml_path = 'examples/gate_ref/gml_boolean_and3_complement_gate.xml'
+        fcs_path = 'examples/gate_ref/data1.fcs'
+        res_path = 'examples/gate_ref/Results_And3.txt'
+
+        gs = GatingStrategy(gml_path)
+        sample = Sample(
+            fcs_path,
+            filter_anomalous_events=False,
+            filter_negative_scatter=False
+        )
+        truth = np.loadtxt(res_path, dtype=np.bool)
+
+        result = gs.gate_sample(sample, 'And3')
+
+        np.testing.assert_array_equal(truth, result['And3'])
