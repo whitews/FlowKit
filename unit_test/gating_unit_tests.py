2ecf3f50c4db832952091767d3b9fef7859162f5
whitews <whitews@gmail.com>
2019-01-23 11:08:56 -0500

    test & data for boolean gate 'not' operation

--- unit_test/gating_unit_tests.py
+++ unit_test/gating_unit_tests.py
@@ -335,3 +335,21 @@ class GatingMLTestCase(unittest.TestCase):
         result = gs.gate_sample(sample, 'And3')
 
         np.testing.assert_array_equal(truth, result['And3'])
+
+    @staticmethod
+    def test_boolean_not1_gate():
+        gml_path = 'examples/gate_ref/gml_boolean_not1_gate.xml'
+        fcs_path = 'examples/gate_ref/data1.fcs'
+        res_path = 'examples/gate_ref/Results_Not1.txt'
+
+        gs = GatingStrategy(gml_path)
+        sample = Sample(
+            fcs_path,
+            filter_anomalous_events=False,
+            filter_negative_scatter=False
+        )
+        truth = np.loadtxt(res_path, dtype=np.bool)
+
+        result = gs.gate_sample(sample, 'Not1')
+
+        np.testing.assert_array_equal(truth, result['Not1'])
