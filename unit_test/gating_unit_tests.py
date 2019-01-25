e85d87241bc286e8c817057ee2bb165426dc49d5
whitews <whitews@gmail.com>
2019-01-25 08:06:39 -0500

    add test for boolean gate w/ parent range gate

--- unit_test/gating_unit_tests.py
+++ unit_test/gating_unit_tests.py
@@ -695,3 +695,21 @@ class GatingMLTestCase(unittest.TestCase):
         result = gs.gate_sample(sample, 'ParAnd2')
 
         np.testing.assert_array_equal(truth, result['ParAnd2'])
+
+    @staticmethod
+    def test_parent_range1_boolean_and3_gate():
+        gml_path = 'examples/gate_ref/gml/gml_parent_range1_boolean_and3_gate.xml'
+        fcs_path = 'examples/gate_ref/data1.fcs'
+        res_path = 'examples/gate_ref/truth/Results_And3.txt'
+
+        gs = GatingStrategy(gml_path)
+        sample = Sample(
+            fcs_path,
+            filter_anomalous_events=False,
+            filter_negative_scatter=False
+        )
+        truth = np.loadtxt(res_path, dtype=np.bool)
+
+        result = gs.gate_sample(sample, 'ParAnd3')
+
+        np.testing.assert_array_equal(truth, result['ParAnd3'])
