457f87e75dc6898fb250bf245c102e9e0b003e63
whitews <whitews@gmail.com>
2019-01-28 09:26:59 -0500

    bump version: 0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='FlowKit',
-    version='0.0.7',
+    version='0.0.8',
     packages=['flowkit', 'flowkit.models'],
     package_data={'flowkit': ['resources/*.xsd']},
     include_package_data=True,
