1d589dd84f4f7c4eb16349e67415d0f5c426d248
whitews <whitews@gmail.com>
2019-01-29 14:11:34 -0500

    more setup metadata

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='FlowKit',
-    version='0.0.9b',
+    version='0.0.9b1',
     packages=find_packages(),
     package_data={'': ['*.xsd']},
     include_package_data=True,
     description='Flow Cytometry Toolkit',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='BSD',
+    url="https://github.com/whitews/flowkit",
     ext_modules=[],
     install_requires=[
         'flowio',
