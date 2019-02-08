422a2cec68f347d0360866cf1f0466534b36ce17
whitews <whitews@gmail.com>
2019-02-08 14:27:53 -0500

    bump version: 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='FlowKit',
-    version='0.0.10b4',
+    version='0.1.0',
     packages=find_packages(),
     package_data={'': ['*.xsd']},
     include_package_data=True,
