b97f1cb68557697b32c3ee1406848b62e9a5ad68
whitews <whitews@gmail.com>
2019-11-14 09:00:41 -0500

    WIP: more GML export tests for rect gates

--- unit_test/export_gml_unit_tests.py
+++ unit_test/export_gml_unit_tests.py
@@ -32,3 +32,41 @@ class ExportGMLTestCase(unittest.TestCase):
         result = gs_out.gate_sample(data1_sample, 'Range1')
 
         np.testing.assert_array_equal(truth, result.get_gate_indices('Range1'))
+
+    @staticmethod
+    def test_rect1_gate():
+        gml_path = 'examples/gate_ref/gml/gml_rect1_gate.xml'
+        res_path = 'examples/gate_ref/truth/Results_Rectangle1.txt'
+
+        gs = GatingStrategy(gml_path)
+
+        out_file = BytesIO()
+
+        gs.export_gml(out_file)
+        out_file.seek(0)
+
+        gs_out = GatingStrategy(out_file)
+
+        result = gs_out.gate_sample(data1_sample, 'Rectangle1')
+        truth = pd.read_csv(res_path, header=None, squeeze=True, dtype='bool').values
+
+        np.testing.assert_array_equal(truth, result.get_gate_indices('Rectangle1'))
+
+    @staticmethod
+    def test_rect2_gate():
+        gml_path = 'examples/gate_ref/gml/gml_rect2_gate.xml'
+        res_path = 'examples/gate_ref/truth/Results_Rectangle2.txt'
+
+        gs = GatingStrategy(gml_path)
+
+        out_file = BytesIO()
+
+        gs.export_gml(out_file)
+        out_file.seek(0)
+
+        gs_out = GatingStrategy(out_file)
+
+        result = gs_out.gate_sample(data1_sample, 'Rectangle2')
+        truth = pd.read_csv(res_path, header=None, squeeze=True, dtype='bool').values
+
+        np.testing.assert_array_equal(truth, result.get_gate_indices('Rectangle2'))
