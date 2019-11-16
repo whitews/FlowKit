f1d3ee480bdd8ccdf143f0746a5ba8ab85f5ea87
whitews <whitews@gmail.com>
2019-11-16 12:06:53 -0500

    add test case for matrices

--- unit_test/unit_tests.py
+++ unit_test/unit_tests.py
@@ -4,6 +4,7 @@ from .sample_unit_tests import LoadSampleTestCase
 from .gating_unit_tests import GatingMLTestCase
 from .prog_gating_unit_tests import GatingTestCase
 from .export_gml_unit_tests import ExportGMLTestCase
+from .matrix_unit_tests import MatrixTestCase
 
 if __name__ == "__main__":
     unittest.main()
