1ca93a822ebf1c9d506b049bf1fad654bc7dfee3
whitews <whitews@gmail.com>
2020-01-31 19:32:54 -0500

    new development version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ utils_extension = Extension(
 
 setup(
     name='FlowKit',
-    version='0.3.2',
+    version='0.3.3b',
     packages=find_packages(),
     package_data={'': ['*.xsd']},
     include_package_data=True,
