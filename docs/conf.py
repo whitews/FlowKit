d03067a8e3c483f056244dd247eec4f231d3cab7
whitews <whitews@gmail.com>
2020-03-21 11:05:43 -0400

    set master_file for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,8 @@ author = 'Scott White'
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
