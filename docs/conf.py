82b1103a0a57eedbf86b4e3d19e450f4fc0dd028
whitews <whitews@gmail.com>
2020-03-24 10:01:10 -0400

    update sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -70,14 +70,13 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'alabaster'
 
 html_theme_options = {
-    'logo': 'flowkit_logo_only.png',
+    'logo': 'flowkit.png',
     'github_user': 'whitews',
     'github_repo': 'flowkit',
-    'github_banner': True,
-    'show_powered_by': False
+    'github_banner': True
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
