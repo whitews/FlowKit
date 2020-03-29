329ff878df24a4d227254b018b56491b04b6976f
whitews <whitews@gmail.com>
2020-03-29 10:10:20 -0400

    custom CSS for docs

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,7 @@ author = 'Scott White'
 # -- General configuration ---------------------------------------------------
 
 master_doc = 'index'
+autodoc_member_order = 'bysource'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -80,3 +81,9 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
