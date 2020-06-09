b181c08b22c19465a26d41869f33d2085163bc0f
whitews <whitews@gmail.com>
2020-06-08 21:46:02 -0400

    expose wsp_transforms module & update transforms notebook

--- flowkit/__init__.py
+++ flowkit/__init__.py
@@ -1,6 +1,6 @@
 from ._models.sample import Sample
 from ._models.transforms.matrix import Matrix
-from ._models.transforms import transforms
+from ._models.transforms import transforms, wsp_transforms
 from ._models.gates import gates
 from ._models.gating_strategy import GatingStrategy
 from ._models.session import Session
