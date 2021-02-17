dda6ac25d769f8a4ae028b8bfa6b4a3253a3a101
whitews <whitews@gmail.com>
2021-02-16 22:16:53 -0500

    add list of FCS standard keywords to sample_utils

--- flowkit/_utils/sample_utils.py
+++ flowkit/_utils/sample_utils.py
@@ -7,6 +7,59 @@ from matplotlib import pyplot
 import numpy as np
 from .._utils import plot_utils
 
+# FCS 3.1 reserves certain keywords as being part of the FCS standard. Some
+# of these are required, and others are optional. However, all of these
+# keywords shall be prefixed by the '$' character. No other keywords shall
+# begin with the '$' character. All keywords are case-insensitive, however
+# most cytometers use all uppercase for keyword strings. FlowKit follows
+# the convention used in FlowIO and internally stores and references all
+# FCS keywords as lowercase for more convenient typing by developers.
+FCS_STANDARD_KEYWORDS = [
+    'beginanalysis',
+    'begindata',
+    'beginstext',
+    'byteord',
+    'datatype',
+    'endanalysis',
+    'enddata',
+    'endstext',
+    'mode',
+    'nextdata',
+    'par',
+    'tot',
+    # start optional standard keywords
+    'abrt',
+    'btim',
+    'cells',
+    'com',
+    'csmode',
+    'csvbits',
+    'cyt',
+    'cytsn',
+    'data',
+    'etim',
+    'exp',
+    'fil',
+    'gate',
+    'inst',
+    'last_modified',
+    'last_modifier',
+    'lost',
+    'op',
+    'originality',
+    'plateid',
+    'platename',
+    'proj',
+    'smno',
+    'spillover',
+    'src',
+    'sys',
+    'timestep',
+    'tr',
+    'vol',
+    'wellid'
+]
+
 
 def _rolling_window(a, window):
     shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
