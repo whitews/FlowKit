d923de99e2a17d64403bbaadaf3305c26d0ee87e
whitews <whitews@gmail.com>
2021-04-05 22:04:52 -0400

    just print Sample file name for verbose mode in GatingStrategy.gate_sample()

--- flowkit/_models/gating_strategy.py
+++ flowkit/_models/gating_strategy.py
@@ -377,7 +377,7 @@ class GatingStrategy(object):
                 continue
 
             if verbose:
-                print("%s: processing gate %s" % (sample, g_id))
+                print("%s: processing gate %s" % (sample.original_filename, g_id))
             if gate.parent is not None and gate.parent in results:
                 parent_results = results[gate.parent]
             else:
