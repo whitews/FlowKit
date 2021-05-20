be41afd319893bbe53ef9c776a5e8a7d796c772a
whitews <whitews@gmail.com>
2021-05-20 12:49:03 -0400

    new release 0.6.1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ reqs = [
 
 setup(
     name='FlowKit',
-    version='0.6.1b0',
+    version='0.6.1',
     packages=find_packages(),
     package_data={'': ['_resources/*.xsd']},
     include_package_data=True,
