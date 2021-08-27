49eb73feed94c3528d05b76845b2f670f8a2ec26
whitews <whitews@gmail.com>
2021-08-26 22:16:14 -0400

    update transform tests

--- flowkit/tests/transform_tests.py
+++ flowkit/tests/transform_tests.py
@@ -25,6 +25,15 @@ class TransformsTestCase(unittest.TestCase):
         self.assertEqual(np.max(xform_events), 1.0)
         self.assertEqual(np.min(xform_events), 0.0)
 
+    def test_transform_sample_linear_1d(self):
+        xform = transforms.LinearTransform('lin', param_t=data1_raw_events.max(), param_a=0.0)
+        xform_lut = {
+            'FL1-H': xform
+        }
+        data1_sample.apply_transform(xform_lut)
+
+        self.assertIsInstance(data1_sample._transformed_events, np.ndarray)
+
     @staticmethod
     def test_inverse_linear_transform():
         xform = transforms.LinearTransform('asinh', param_t=10000, param_a=0)
@@ -42,6 +51,23 @@ class TransformsTestCase(unittest.TestCase):
 
         self.assertIsInstance(xform_events, np.ndarray)
         self.assertRaises(AssertionError, np.testing.assert_array_equal, raw_events, xform_events)
+
+    def test_transform_sample_asinh_1d(self):
+        xform = transforms.AsinhTransform('asinh', param_t=10000, param_m=4.5, param_a=0)
+        xform_lut = {
+            'FL1-H': xform
+        }
+        data1_sample.apply_transform(xform_lut)
+
+        self.assertIsInstance(data1_sample._transformed_events, np.ndarray)
+
+    @staticmethod
+    def test_inverse_asinh_transform():
+        xform = transforms.AsinhTransform('asinh', param_t=10000, param_m=4.5, param_a=0)
+        y = xform.apply(test_data_range1)
+        x = xform.inverse(y)
+
+        np.testing.assert_array_almost_equal(test_data_range1, x, decimal=10)
 
     def test_transform_ratio_raises_not_implemented(self):
         ratio_dims = ['FL1-H', 'FL2-H']
@@ -73,19 +99,20 @@ class TransformsTestCase(unittest.TestCase):
 
         self.assertIsInstance(data1_sample._transformed_events, np.ndarray)
 
+    def test_transform_sample_log_1d(self):
+        xform = transforms.LogTransform('log', param_t=10000, param_m=4.5)
+        xform_lut = {
+            'FL1-H': xform
+        }
+        data1_sample.apply_transform(xform_lut)
+
+        self.assertIsInstance(data1_sample._transformed_events, np.ndarray)
+
     @staticmethod
     def test_inverse_log_transform():
         xform = transforms.LogTransform('log', param_t=10000, param_m=4.5)
-        y = xform.apply(test_data_range1.reshape(-1, 1))
-        x = xform.inverse(y)[:, 0]
-
-        np.testing.assert_array_almost_equal(test_data_range1, x, decimal=10)
-
-    @staticmethod
-    def test_inverse_asinh_transform():
-        xform = transforms.AsinhTransform('asinh', param_t=10000, param_m=4.5, param_a=0)
-        y = xform.apply(test_data_range1.reshape(-1, 1))
-        x = xform.inverse(y)[:, 0]
+        y = xform.apply(test_data_range1)
+        x = xform.inverse(y)
 
         np.testing.assert_array_almost_equal(test_data_range1, x, decimal=10)
 
@@ -95,11 +122,20 @@ class TransformsTestCase(unittest.TestCase):
 
         self.assertIsInstance(data1_sample._transformed_events, np.ndarray)
 
+    def test_transform_sample_logicle_1d(self):
+        xform = transforms.LogicleTransform('logicle', param_t=10000, param_w=0.5, param_m=4.5, param_a=0)
+        xform_lut = {
+            'FL1-H': xform
+        }
+        data1_sample.apply_transform(xform_lut)
+
+        self.assertIsInstance(data1_sample._transformed_events, np.ndarray)
+
     @staticmethod
     def test_inverse_logicle_transform():
         xform = transforms.LogicleTransform('logicle', param_t=10000, param_w=0.5, param_m=4.5, param_a=0)
-        y = xform.apply(test_data_range1.reshape(-1, 1))
-        x = xform.inverse(y)[:, 0]
+        y = xform.apply(test_data_range1)
+        x = xform.inverse(y)
 
         np.testing.assert_array_almost_equal(test_data_range1, x, decimal=10)
 
@@ -109,11 +145,20 @@ class TransformsTestCase(unittest.TestCase):
 
         self.assertIsInstance(data1_sample._transformed_events, np.ndarray)
 
+    def test_transform_sample_hyperlog_1d(self):
+        xform = transforms.HyperlogTransform('hyper', param_t=10000, param_w=0.5, param_m=4.5, param_a=0)
+        xform_lut = {
+            'FL1-H': xform
+        }
+        data1_sample.apply_transform(xform_lut)
+
+        self.assertIsInstance(data1_sample._transformed_events, np.ndarray)
+
     @staticmethod
     def test_inverse_hyperlog_transform():
         xform = transforms.HyperlogTransform('hyperlog', param_t=10000, param_w=0.5, param_m=4.5, param_a=0)
-        y = xform.apply(test_data_range1.reshape(-1, 1))
-        x = xform.inverse(y)[:, 0]
+        y = xform.apply(test_data_range1)
+        x = xform.inverse(y)
 
         np.testing.assert_array_almost_equal(test_data_range1, x, decimal=10)
 
@@ -122,3 +167,13 @@ class TransformsTestCase(unittest.TestCase):
         data1_sample.apply_transform(xform)
 
         self.assertIsInstance(data1_sample._transformed_events, np.ndarray)
+
+    def test_transform_sample_wsp_log_1d(self):
+        xform = transforms.WSPLogTransform('wsp_log', offset=0.5, decades=4.5)
+        xform_lut = {
+            'FL1-H': xform
+        }
+        data1_sample.apply_transform(xform_lut)
+
+        self.assertIsInstance(data1_sample._transformed_events, np.ndarray)
+
