1ff769b9e98343df0fb1d934e3f30c01d6ea2a9d
whitews <whitews@gmail.com>
2021-08-27 12:41:58 -0400

    bump version to next micro preview

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ reqs = [
 
 setup(
     name='FlowKit',
-    version='0.6.2',
+    version='0.6.90b',
     packages=find_packages(exclude=["flowkit/tests/"]),
     package_data={'': ['_resources/*.xsd']},
     include_package_data=True,
