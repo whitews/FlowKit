7837f4902083f4555f7e6abe8bfbf833eb3ee8d9
whitews <whitews@gmail.com>
2021-09-28 10:52:19 -0400

    remove test for anomaly detection, update beta version

--- flowkit/tests/sample_tests.py
+++ flowkit/tests/sample_tests.py
@@ -458,20 +458,3 @@ class SampleTestCase(unittest.TestCase):
         self.assertEqual(len(common_idx), 0)
 
         self.assertEqual(sample.negative_scatter_indices.shape[0], 2)
-
-    def test_filter_anomalous_events(self):
-        # there are 2 negative SSC-A events in this file (of 65016 total events)
-        fcs_file_path = "examples/data/100715.fcs"
-        sample = Sample(fcs_path_or_data=fcs_file_path)
-        sample.subsample_events(50000)
-        sample.filter_anomalous_events(reapply_subsample=False)
-
-        # using the default seed, the 2 negative events are in the subsample
-        common_idx = np.intersect1d(sample.subsample_indices, sample.anomalous_indices)
-        self.assertGreater(len(common_idx), 0)
-
-        sample.filter_anomalous_events(reapply_subsample=True)
-        common_idx = np.intersect1d(sample.subsample_indices, sample.anomalous_indices)
-        self.assertEqual(len(common_idx), 0)
-
-        self.assertGreater(sample.anomalous_indices.shape[0], 0)
--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ reqs = [
 
 setup(
     name='FlowKit',
-    version='0.7.1b1',
+    version='0.7.1b2',
     packages=find_packages(exclude=["flowkit/tests/"]),
     package_data={'': ['_resources/*.xsd']},
     include_package_data=True,
