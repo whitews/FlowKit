91b9271457ab834127f522c5912383800b6bd6bb
whitews <whitews@gmail.com>
2022-04-21 13:38:44 -0400

    update setup to remove C extension, drop Python 3.6 support

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """
 Setup script for the FlowKit package
 """
-from setuptools import dist, setup, find_packages, Extension
+from setuptools import setup, find_packages
 
 # read in version string
 VERSION_FILE = 'flowkit/_version.py'
@@ -11,26 +11,8 @@ exec(open(VERSION_FILE).read())
 if __version__ is None:
     raise RuntimeError("__version__ string not found in file %s" % VERSION_FILE)
 
-# NumPy is needed to build
-# This retrieves a version at build time compatible with run time version
-dist.Distribution().fetch_build_eggs(['numpy>=1.19'])
-
-# override inspection for import not at top of file
-# this has to be imported here, after fetching the NumPy egg
-import numpy as np  # noqa: E402
-
 with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
-
-utils_extension = Extension(
-    'flowkit._utils_c',
-    sources=[
-        'flowkit/utils_c_ext/_utils.c',
-        'flowkit/utils_c_ext/utils.c'
-    ],
-    include_dirs=[np.get_include(), 'flowkit/utils_c_ext'],
-    extra_compile_args=['-std=c99']
-)
 
 reqs = [
     'flowio',
@@ -40,9 +22,9 @@ reqs = [
     'statsmodels',
     'seaborn',
     'pandas',
-    'numpy',
+    'numpy>=1.19',
     'lxml',
-    'bokeh',
+    'bokeh>=2,<3',
     'anytree',
     'networkx',
     'psutil'
@@ -60,12 +42,11 @@ setup(
     author="Scott White",
     license='BSD',
     url="https://github.com/whitews/flowkit",
-    ext_modules=[utils_extension],
+    ext_modules=[],
     install_requires=reqs,
     classifiers=[
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7'
     ]
 )
