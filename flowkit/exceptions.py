c9d3a7c05e5d272dfa261f27823d8e88f7c5f80b
whitews <whitews@gmail.com>
2022-08-05 16:25:52 -0400

    add more custom exceptions

--- flowkit/exceptions.py
+++ flowkit/exceptions.py
@@ -8,3 +8,18 @@ This module contains the set of FlowKit exceptions.
 class GateTreeError(Exception):
     """An error modifying a GatingStrategy gate tree occurred."""
     pass
+
+
+class GateReferenceError(Exception):
+    """An error referencing a Gate instance occurred."""
+    pass
+
+
+class QuadrantReferenceError(Exception):
+    """An error when requesting a single Quadrant as a gate in a GatingStrategy."""
+    pass
+
+
+class FlowJoWSPParsingError(Exception):
+    """An error parsing a FlowJo .wsp file"""
+    pass
