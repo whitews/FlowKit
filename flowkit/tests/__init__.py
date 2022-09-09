2554e963de770212c7aa0a7c9f14c299eff9932c
whitews <whitews@gmail.com>
2022-09-09 12:11:40 -0400

    remove test & test data related to comp from beads

--- flowkit/tests/__init__.py
+++ flowkit/tests/__init__.py
@@ -5,7 +5,6 @@ import unittest
 
 from .sample_tests import SampleTestCase
 from .sample_export_tests import SampleExportTestCase
-from .sample_utils_tests import SampleUtilsTestCase
 from .gatingml_tests import GatingMLTestCase
 from .gating_strategy_prog_gate_tests import GatingTestCase
 from .export_gml_tests import ExportGMLTestCase
