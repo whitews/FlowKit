3c011982d10e99947cff111ffbba1a0bf579be13
whitews <whitews@gmail.com>
2022-10-11 11:35:11 -0400

    make all exceptions a subclass of FlowKitException, add new warnings we'll need later

--- flowkit/exceptions.py
+++ flowkit/exceptions.py
@@ -5,21 +5,36 @@ This module contains the set of FlowKit exceptions.
 """
 
 
-class GateTreeError(Exception):
+class FlowKitWarning(Warning):
+    """A generic FlowKit warning"""
+    pass
+
+
+class FlowKitException(Exception):
+    """A generic FlowKit exception"""
+    pass
+
+
+class GateTreeError(FlowKitException):
     """An error modifying a GatingStrategy gate tree occurred."""
     pass
 
 
-class GateReferenceError(Exception):
+class GateReferenceError(FlowKitException):
     """An error referencing a Gate instance occurred."""
     pass
 
 
-class QuadrantReferenceError(Exception):
+class QuadrantReferenceError(FlowKitException):
     """An error when requesting a single Quadrant as a gate in a GatingStrategy."""
     pass
 
 
-class FlowJoWSPParsingError(Exception):
+class FlowJoWSPParsingError(FlowKitException):
     """An error parsing a FlowJo .wsp file"""
+    pass
+
+
+class FlowJoWSPParsingWarning(FlowKitWarning):
+    """A warning when parsing a FlowJo .wsp file"""
     pass
