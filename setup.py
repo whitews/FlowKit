617e098934ec850275f1bc06cb8110b2e99788f8
whitews <whitews@gmail.com>
2024-03-27 23:32:53 -0400

    drop Python 3.7, add 3.11 & 3.12

--- setup.py
+++ setup.py
@@ -44,9 +44,10 @@ setup(
     ext_modules=[],
     install_requires=reqs,
     classifiers=[
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.8'
     ]
 )
