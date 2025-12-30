68e0b699b541f191e5afd81d0f054dc3232caec1
whitews <whitews@gmail.com>
2025-12-29 23:08:02 -0500

    add Python 3.14 to supported versions in setup.py

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
     ext_modules=[],
     install_requires=reqs,
     classifiers=[
+        'Programming Language :: Python :: 3.14',
         'Programming Language :: Python :: 3.13',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.11',
